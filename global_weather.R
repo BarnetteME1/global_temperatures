b61b5d6f4b078891cfd7d76943ea86fdf376b77f
BarnetteME1 <BarnetteME1@gmail.com>
2016-03-22 16:21:10 -0400

    trying to figure out maps

--- global_weather.R
+++ global_weather.R
@@ -1,3 +1,5 @@
+#importing libraries
+
 library(ggplot2)
 
 gt_by_city <- read.csv('~/global_weather_R/sv_files/GlobalLandTemperaturesByCity.csv')
@@ -7,8 +9,18 @@ gt_by_state <- read.csv('~/global_weather_R/sv_files/GlobalLandTemperaturesBySta
 gt <- read.csv('~/global_weather_R/sv_files/GlobalTemperatures.csv')
 gt$dt <- as.Date(gt$dt, format = "%Y-%m-%d")
 
+#plotting global temperatures
+
 ggplot(aes(x = dt, y = LandAverageTemperature), data = gt) + 
   geom_line() + 
   scale_x_date(date_breaks = '10 years', limits = as.Date(c("1750-01-01", "1800-01-01")))
 
-summary(gt)
+#seeing if I can put temperatures on a map
+
+by(gt_by_country$AverageTemperature, gt_by_country$Country, summary)
+seq()
+
+library(maps)
+png('map_1europe.png')
+map('world')
+dev.off()
